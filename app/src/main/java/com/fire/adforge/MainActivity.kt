8a203930ea4c4ac4300fd7223c294b4b5f19403a
Commander Fire <fire@adforge.dev>
2025-07-18 18:38:20 -0400

    Kit165: Inject FirebaseAuth into UserViewModel and wire in MainActivity

--- app/src/main/java/com/fire/adforge/MainActivity.kt
+++ app/src/main/java/com/fire/adforge/MainActivity.kt
@@ -1,11 +1,11 @@
 ﻿package com.fire.adforge
 
-import android.os.Bundle`nimport com.google.firebase.auth.FirebaseAuth
-import androidx.activity.ComponentActivity`nimport com.google.firebase.auth.FirebaseAuth
-import androidx.activity.compose.setContent`nimport com.google.firebase.auth.FirebaseAuth
-import androidx.compose.material3.MaterialTheme`nimport com.google.firebase.auth.FirebaseAuth
-import androidx.navigation.compose.rememberNavController`nimport com.google.firebase.auth.FirebaseAuth
-import com.fire.adforge.ui.MainScreen`nimport com.google.firebase.auth.FirebaseAuth
+import android.os.Bundle`nimport com.google.firebase.auth.FirebaseAuth`nimport com.fire.adforge.viewmodel.UserViewModel`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.activity.ComponentActivity`nimport com.google.firebase.auth.FirebaseAuth`nimport com.fire.adforge.viewmodel.UserViewModel`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.activity.compose.setContent`nimport com.google.firebase.auth.FirebaseAuth`nimport com.fire.adforge.viewmodel.UserViewModel`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.compose.material3.MaterialTheme`nimport com.google.firebase.auth.FirebaseAuth`nimport com.fire.adforge.viewmodel.UserViewModel`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.compose.rememberNavController`nimport com.google.firebase.auth.FirebaseAuth`nimport com.fire.adforge.viewmodel.UserViewModel`nimport com.google.firebase.auth.FirebaseAuth
+import com.fire.adforge.ui.MainScreen`nimport com.google.firebase.auth.FirebaseAuth`nimport com.fire.adforge.viewmodel.UserViewModel`nimport com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/fire/adforge/viewmodel/UserViewModel.kt
@@ -0,0 +1,10 @@
+﻿package com.fire.adforge.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class UserViewModel(private val auth: FirebaseAuth) : ViewModel() {
+    fun getCurrentUserUid(): String? {
+        return auth.currentUser?.uid
+    }
+}

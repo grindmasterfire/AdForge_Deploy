55628fadef7abdcb389f0a03a4ff4e0ff1a7d20a
Commander Fire <fire@adforge.dev>
2025-07-18 18:52:47 -0400

    Kit173: Add Invite icon to MainScreen TopBar

--- app/src/main/java/com/fire/adforge/ui/MainScreen.kt
+++ app/src/main/java/com/fire/adforge/ui/MainScreen.kt
@@ -1,11 +1,11 @@
 ﻿package com.fire.adforge.ui
 
-import androidx.compose.foundation.layout.*`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List
-import androidx.compose.material3.*`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List
-import androidx.compose.runtime.*`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List
-import androidx.compose.ui.Alignment`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List
-import androidx.compose.ui.Modifier`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List
-import androidx.compose.ui.unit.dp`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List
+import androidx.compose.foundation.layout.*`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List`nimport androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.material3.*`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List`nimport androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.runtime.*`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List`nimport androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.ui.Alignment`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List`nimport androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.ui.Modifier`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List`nimport androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.ui.unit.dp`nimport com.fire.adforge.ui.CountdownBanner`nimport com.fire.adforge.backend.CrewBadgeBinder`nimport androidx.compose.material.icons.filled.List`nimport androidx.compose.material.icons.filled.PersonAdd
 
 @Composable
 fun MainScreen() {

2b15c6dfa5d6bc01b2ad503e800ca47126b0e9f6
Commander Fire <fire@adforge.dev>
2025-07-18 16:40:16 -0400

    Kit157: Add booster_store route to NavGraph for BoosterStoreScreen

--- app/src/main/java/com/fire/adforge/ui/NavGraph.kt
+++ app/src/main/java/com/fire/adforge/ui/NavGraph.kt
@@ -1,13 +1,13 @@
 ﻿package com.fire.adforge.ui
 
-import androidx.compose.runtime.Composable`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen
-import androidx.navigation.NavHostController`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen
-import androidx.navigation.compose.NavHost`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen
-import androidx.navigation.compose.composable`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen
+import androidx.compose.runtime.Composable`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen`nimport com.fire.adforge.ui.BoosterStoreScreen
+import androidx.navigation.NavHostController`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen`nimport com.fire.adforge.ui.BoosterStoreScreen
+import androidx.navigation.compose.NavHost`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen`nimport com.fire.adforge.ui.BoosterStoreScreen
+import androidx.navigation.compose.composable`nimport com.fire.adforge.ui.MailboxScreen`nimport com.fire.adforge.ui.CrewJoinScreen`nimport com.fire.adforge.ui.CrewLeaderboardScreen`nimport com.fire.adforge.ui.BoosterStoreScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "main") {`n composable("mailbox") {`n composable("crew") {`n composable("crew_leaderboard") { CrewLeaderboardScreen() } CrewJoinScreen() } MailboxScreen() }
+    NavHost(navController = navController, startDestination = "main") {`n composable("mailbox") {`n composable("crew") {`n composable("crew_leaderboard") {`n composable("booster_store") { BoosterStoreScreen() } CrewLeaderboardScreen() } CrewJoinScreen() } MailboxScreen() }
         composable("main") { MainScreen(navController) }
         composable("wallet") { WalletScreen() }
         composable("wall") { PersonalWallScreen() }

e18048dd795a4b158d6fa7f81d23388b69ad8b5f
Commander Fire <fire@adforge.dev>
2025-07-18 18:42:11 -0400

    Kit167: Add CoinLedgerLogger utility to write entries to Firestore

---
+++ app/src/main/java/com/fire/adforge/util/CoinLedgerLogger.kt
@@ -0,0 +1,13 @@
+﻿package com.fire.adforge.util
+
+import com.fire.adforge.model.CoinLedger
+import com.google.firebase.firestore.FirebaseFirestore
+
+object CoinLedgerLogger {
+    private val db = FirebaseFirestore.getInstance()
+    private val logRef = db.collection(\"coin_ledger\")
+
+    fun log(entry: CoinLedger) {
+        logRef.add(entry)
+    }
+}

1cc16498846c22342986d222f45a41ca1847c82c
Commander Fire <fire@adforge.dev>
2025-07-18 16:11:51 -0400

    Kit136: Pass navController from MainActivity to NavGraph for route injection

--- app/src/main/java/com/fire/adforge/MainActivity.kt
+++ app/src/main/java/com/fire/adforge/MainActivity.kt
@@ -1,15 +1,15 @@
 ﻿package com.fire.adforge
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.*
-import com.fire.adforge.ui.MainScreen
+import android.os.Bundle`nimport androidx.navigation.compose.rememberNavController
+import androidx.activity.ComponentActivity`nimport androidx.navigation.compose.rememberNavController
+import androidx.activity.compose.setContent`nimport androidx.navigation.compose.rememberNavController
+import androidx.compose.material3.*`nimport androidx.navigation.compose.rememberNavController
+import com.fire.adforge.ui.MainScreen`nimport androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent { val navController = rememberNavController()
             MaterialTheme {
                 MainScreen()
             }

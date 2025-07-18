da336db9f4bd911c27014fe4c2f04b8df5b48753
Commander Fire <fire@adforge.dev>
2025-07-18 16:37:17 -0400

    Kit155: Add BoosterWriter to log milestone accelerators in Firestore

---
+++ app/src/main/java/com/fire/adforge/backend/BoosterWriter.kt
@@ -0,0 +1,19 @@
+﻿package com.fire.adforge.backend
+
+import com.fire.adforge.model.BoosterOffer
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+object BoosterWriter {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+
+    fun logBoosterPurchase(offer: BoosterOffer) {
+        val uid = auth.currentUser?.uid ?: return
+        db.collection("users")
+            .document(uid)
+            .collection("boosters")
+            .add(offer)
+    }
+}

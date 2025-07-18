27d5f0a018ed33efb76ddb1f2278cca27b86e4ab
Commander Fire <fire@adforge.dev>
2025-07-18 18:34:10 -0400

    Kit165: Inject FirebaseAuth into UserViewModel and wire from MainActivity

--- app/src/main/java/com/fire/adforge/MainActivity.kt
+++ app/src/main/java/com/fire/adforge/MainActivity.kt
@@ -1,16 +1,16 @@
 ﻿package com.fire.adforge
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.navigation.compose.rememberNavController
-import com.fire.adforge.ui.MainScreen
+import android.os.Bundle`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.activity.ComponentActivity`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.activity.compose.setContent`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.compose.material3.MaterialTheme`nimport com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.compose.rememberNavController`nimport com.google.firebase.auth.FirebaseAuth
+import com.fire.adforge.ui.MainScreen`nimport com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        val auth = FirebaseAuth.getInstance()`n        setContent {
             val navController = rememberNavController()
             MaterialTheme {
                 MainScreen(navController = navController)

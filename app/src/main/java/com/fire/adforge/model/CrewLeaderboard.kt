04446df61451934cffa430a9bb9a4a938bf223f4
Commander Fire <fire@adforge.dev>
2025-07-18 16:31:43 -0400

    Kit149: Add CrewLeaderboard.kt model to support competitive ranking

---
+++ app/src/main/java/com/fire/adforge/model/CrewLeaderboard.kt
@@ -0,0 +1,9 @@
+﻿package com.fire.adforge.model
+
+data class CrewLeaderboard(
+    val crewName: String = "",
+    val totalEarnings: Int = 0,
+    val memberCount: Int = 0,
+    val multiplier: Double = 1.0,
+    val rank: Int = 0
+)

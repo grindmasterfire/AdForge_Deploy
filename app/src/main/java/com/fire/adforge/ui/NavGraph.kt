191146251956cd2fe77a65a33a4aadea9b1b779c
Commander Fire <fire@adforge.dev>
2025-07-18 16:09:51 -0400

    Kit133: Add mailbox route to NavGraph

--- app/src/main/java/com/fire/adforge/ui/NavGraph.kt
+++ app/src/main/java/com/fire/adforge/ui/NavGraph.kt
@@ -1,13 +1,13 @@
 ﻿package com.fire.adforge.ui
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.compose.runtime.Composable`nimport com.fire.adforge.ui.MailboxScreen
+import androidx.navigation.NavHostController`nimport com.fire.adforge.ui.MailboxScreen
+import androidx.navigation.compose.NavHost`nimport com.fire.adforge.ui.MailboxScreen
+import androidx.navigation.compose.composable`nimport com.fire.adforge.ui.MailboxScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "main") {
+    NavHost(navController = navController, startDestination = "main") {`n composable("mailbox") { MailboxScreen() }
         composable("main") { MainScreen(navController) }
         composable("wallet") { WalletScreen() }
         composable("wall") { PersonalWallScreen() }

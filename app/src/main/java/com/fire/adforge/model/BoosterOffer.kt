9aa539b5af8f8cdb76b2cb6afdbb901df7d3b045
Commander Fire <fire@adforge.dev>
2025-07-18 16:36:36 -0400

    Kit154: Create BoosterOffer.kt model for milestone speed-up offers

---
+++ app/src/main/java/com/fire/adforge/model/BoosterOffer.kt
@@ -0,0 +1,10 @@
+﻿package com.fire.adforge.model
+
+data class BoosterOffer(
+    val offerId: String = "",
+    val title: String = "",
+    val provider: String = "",
+    val coinCost: Int = 0,
+    val boostPercent: Int = 5,
+    val timestamp: Long = System.currentTimeMillis()
+)

85f4a67b1a754401f834361037e09f0c62cc5e55
Commander Fire <fire@adforge.dev>
2025-07-21 19:41:54 -0400

    Kit478: Launcher splash screen to MainActivity transition

---
+++ app/src/main/java/com/fire/adforge/SplashActivity.kt
@@ -0,0 +1,15 @@
+﻿package com.fire.adforge
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}

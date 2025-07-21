71298c6fb0a4a60eac836fa4328ac16887d34521
Commander Fire <fire@adforge.dev>
2025-07-21 12:35:24 -0400

    Kit426: Liveness popup now triggers at selected time during autoplay session

--- app/src/main/java/com/fire/adforge/viewmodel/BreadloopViewModel.kt
+++ app/src/main/java/com/fire/adforge/viewmodel/BreadloopViewModel.kt
@@ -14,7 +14,18 @@ class BreadloopViewModel : ViewModel() {
     val walletCoins = MutableStateFlow(0)
     val isSessionEligible = MutableStateFlow(false)
 
+    val currentPlaybackTime = MutableStateFlow(0L) // in milliseconds
+    val selectedLivenessTime = MutableStateFlow(120000L) // default: 2 min
+
+    fun updatePlaybackTime(ms: Long) {
+        currentPlaybackTime.value = ms
+        if (!livenessTriggered.value && ms >= selectedLivenessTime.value) {
+            triggerLivenessMoment()
+        }
+    }
+
     fun triggerLivenessMoment() {
+        livenessTriggered.value = true
         showGratitudePopup.value = true
     }
 

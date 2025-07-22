8eae570cf272eb4e92e8c028f236a04ef40a5a27
Commander Fire <fire@adforge.dev>
2025-07-22 09:23:19 -0400

    Kit506: Fixed plugin classpath repo error by restoring buildscript repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 ﻿buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath(kotlin("gradle-plugin", version = "1.9.0"))

eb3e607e5489820c7f5e018048a0cb8e92263f50
Commander Fire <fire@adforge.dev>
2025-07-22 10:10:10 -0400

    Kit522: Upgraded Kotlin plugin to 1.9.20 for Compose compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
-        classpath(kotlin("gradle-plugin", version = "1.9.0"))
+        classpath(kotlin("gradle-plugin", version = "1.9.20"))
     }
 }

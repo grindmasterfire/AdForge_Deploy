4ab396ba7c547aa765cfd53715f563f815f6daa3
Commander Fire <fire@adforge.dev>
2025-07-22 10:03:42 -0400

    Kit520: Rewrote app/build.gradle.kts to fix broken AppCompat inject

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,7 @@ android {
     }
 }
 
-dependencies {\n    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.01.00"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -37,5 +36,6 @@ dependencies {\n    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
-

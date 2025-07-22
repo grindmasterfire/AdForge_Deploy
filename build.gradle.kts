03c275cd1927fec87325f361e347a831ebe063c3
Commander Fire <fire@adforge.dev>
2025-07-22 09:20:49 -0400

    Kit504: Moved repositories to settings.gradle.kts to comply with Gradle 8+

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,6 @@
 ﻿buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath(kotlin("gradle-plugin", version = "1.9.0"))
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,13 +7,11 @@
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "AdForge_Android3"
 include(":app")
-

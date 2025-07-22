5607bde3dd80a77ecea5802ad5e3c12cdaa5b672
Commander Fire <fire@adforge.dev>
2025-07-22 15:45:01 -0400

    Kit554-R: Recover core.meta.BuildMeta.kt and finalize metadata injection

---
+++ app/src/main/java/com/fire/adforge/core/meta/BuildMeta.kt
@@ -0,0 +1,9 @@
+﻿package com.fire.adforge.core.meta
+
+object BuildMeta {
+    const val VERSION_CODE = 1001
+    const val VERSION_NAME = "Alpha 1.0"
+    const val CODEX_TAG = "Commander Cipher Protocol v17-A"
+    const val COMMIT_MESSAGE = "Breadloop Complete. All Systems Go."
+    const val BUILD_DATE = "2025-07-22"
+}

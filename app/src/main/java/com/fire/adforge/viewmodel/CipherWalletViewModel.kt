93d5bac8bb1f8cefe3b610b8b2dd78201eb8eb7a
Commander Fire <fire@adforge.dev>
2025-07-22 19:02:58 -0400

    Kit582: Patched CipherWalletViewModel to bypass Kotlin crash

---
+++ app/src/main/java/com/fire/adforge/viewmodel/CipherWalletViewModel.kt
@@ -0,0 +1,10 @@
+﻿package com.fire.adforge.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class CipherWalletViewModel : ViewModel() {
+    private val _walletStatus = MutableLiveData<String>("CipherWallet disabled (patched)")
+    val walletStatus: LiveData<String> = _walletStatus
+}

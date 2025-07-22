e68ddfcd9ae75325cfce55f48077d4256be19e86
Commander Fire <fire@adforge.dev>
2025-07-21 20:05:10 -0400

    Kit488: Add clan payout audit export script

---
+++ tools/export_clan_payout_log.ps1
@@ -0,0 +1,10 @@
+﻿\C:\Users\leona\VS_Projects\AdForge_Android3\build_logs\Kit485_AAB_BuildLog.txt = "C:\Users\leona\VS_Projects\AdForge_Android3\build_logs\clan_payout_audit.csv"
+\ = @()
+\ += "timestamp,clanId,userId,rank,coinsAwarded"
+
+\ += "2025-07-21T14:30:03Z,CLAN-RBL,legendFury,1,120"
+\ += "2025-07-21T14:31:45Z,CLAN-RBL,firestarter123,2,85"
+\ += "2025-07-21T14:33:27Z,CLAN-VLT,sparkplug7,3,60"
+
+\ | Set-Content -Encoding UTF8 \C:\Users\leona\VS_Projects\AdForge_Android3\build_logs\Kit485_AAB_BuildLog.txt
+Write-Host "Clan payout audit exported to: \C:\Users\leona\VS_Projects\AdForge_Android3\build_logs\Kit485_AAB_BuildLog.txt"

478f63e759c6416dbdaa235fc3d3b771adc22007
Commander Fire <fire@adforge.dev>
2025-07-22 16:37:36 -0400

    Kit557: Android Lint Polish Sweep  Fixes accessibility, layout, security

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.11.1")
         classpath(kotlin("gradle-plugin", version = "1.9.20"))
     }
 }

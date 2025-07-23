f76a0448f7b414dcdc400a1d6cdc497c5e6d7c84
Commander Fire <fire@adforge.dev>
2025-07-23 18:48:37 -0400

    Kit794: Fix plugin block with required version numbers for Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,34 +1,5 @@
 ﻿plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
-}
-
-android {
-
-repositories { google(); mavenCentral() }
-    namespace = "com.adforge.app"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.fire.adforge"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-}
-
-dependencies {
-    implementation("com.google.firebase:firebase-analytics:21.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    id("com.android.application") version "8.4.1"
+    id("org.jetbrains.kotlin.android") version "1.9.10"
+    id("com.google.gms.google-services") version "4.4.1"
 }

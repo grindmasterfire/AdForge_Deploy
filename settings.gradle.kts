aa66361aabb1e68d2ff7741935db4f22187de3f3
Commander Fire <fire@adforge.dev>
2025-07-23 11:37:21 -0400

    Kit747: Ensure :app module is included in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,2 @@
-﻿pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "AdForge_Android3"
+﻿rootProject.name = "AdForge_Android3"
 include(":app")

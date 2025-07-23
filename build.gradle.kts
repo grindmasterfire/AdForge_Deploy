b736fe805bada181676498c902bdbf659ed3e343
Commander Fire <fire@adforge.dev>
2025-07-23 12:38:20 -0400

    Kit762: Activated com.android.application plugin with apply=true to enable Android app build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 ﻿plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
+    id("com.android.application") version "8.2.2" apply true
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply true
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
 }
+
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")

8be8fd712a507d476670936f86d1d725c46f2dc5
Commander Fire <fire@adforge.dev>
2025-07-25 18:18:14 -0400

    Kit545: Quarantined inline lambdas in NavHost, moved to named composables

--- app/src/main/java/com/fire/adforge/navigation/AdForgeNavHost.kt
+++ app/src/main/java/com/fire/adforge/navigation/AdForgeNavHost.kt
@@ -1,27 +1,51 @@
 ﻿package com.fire.adforge.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun AdForgeNavHost(startDestination: String = "home") {
     val navController = rememberNavController()
     println("AdForgeNavHost active")
     NavHost(navController = navController, startDestination = startDestination) {
-        adForgeGraph(navController)
+        addAdForgeGraph(navController)
     }
 }
 
-// Separated logic to avoid recursion
-fun NavGraphBuilder.adForgeGraph(navController: androidx.navigation.NavHostController) {
-    composable("home") {
-        HomeScreen(navController)
-    }
-    composable("rewards") {
-        RewardsScreen(navController)
-    }
-    // Add other routes here without calling AdForgeNavHost()
+fun NavGraphBuilder.addAdForgeGraph(navController: NavHostController) {
+    composable("home") { AdForgeHomeDestination(navController) }
+    composable("rewards") { AdForgeRewardsDestination(navController) }
+    composable("milestones") { AdForgeMilestoneDestination(navController) }
+    composable("payout") { AdForgePayoutDestination(navController) }
+    composable("crew") { AdForgeCrewDestination(navController) }
+    // Add more routed screens here as needed
+}
+
+@Composable
+fun AdForgeHomeDestination(navController: NavHostController) {
+    HomeScreen(navController)
+}
+
+@Composable
+fun AdForgeRewardsDestination(navController: NavHostController) {
+    RewardsScreen(navController)
+}
+
+@Composable
+fun AdForgeMilestoneDestination(navController: NavHostController) {
+    MilestoneScreen(navController)
+}
+
+@Composable
+fun AdForgePayoutDestination(navController: NavHostController) {
+    PayoutScreen(navController)
+}
+
+@Composable
+fun AdForgeCrewDestination(navController: NavHostController) {
+    CrewScreen(navController)
 }

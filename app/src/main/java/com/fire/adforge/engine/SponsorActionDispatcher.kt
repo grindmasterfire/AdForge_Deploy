7da32baf5f6864acbc807735e7f4df8ca86bee16
Commander Fire <fire@adforge.dev>
2025-07-25 11:20:13 -0400

    Kit509: SponsorBoardScreen click logic wired to SponsorActionDispatcher

---
+++ app/src/main/java/com/fire/adforge/engine/SponsorActionDispatcher.kt
@@ -0,0 +1,8 @@
+package com.fire.adforge.engine
+
+object SponsorActionDispatcher {
+    fun launch(name: String) {
+        // Placeholder: Hook into SDK or open sponsor page
+        println("Launched sponsor logic for \ChartboostMediation")
+    }
+}

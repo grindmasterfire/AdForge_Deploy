b51b3c89ce27e2dbf0292f271ae301fe37b12fe1
Commander Fire <fire@adforge.dev>
2025-07-25 13:18:49 -0400

    Kit518: Injected pluginManagement block with versions for Android and Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.1.1"
+        id("org.jetbrains.kotlin.android") version "1.9.10"
+    }
+}
 
 include(":app")
+
